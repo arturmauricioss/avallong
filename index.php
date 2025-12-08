0c55d9376d0d3fbe4d84449362b852dc77890052
Artur Maurício <arturmauricioss@outlook.com>
2025-12-08 08:48:42 -0300

    adicionando a logo

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Avallong</title>
+    <link rel="icon" href="assets/img/logocolorida.png" type="image/x-icon">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+    <header></header>
+    <nav></nav>
+    <aside></aside>
+    <section></section>
+    <article></article>
+    <footer></footer>
+  
+</body>
+</html>
