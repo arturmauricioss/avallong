d34f3d8926104e41f5f9a077292c7a69d2e15742
Artur Maurício <arturmauricioss@outlook.com>
2025-12-08 10:06:05 -0300

    adicionando um titulo

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <link rel="icon" href="assets/img/logocolorida.png" type="image/x-icon">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
-    <header></header>
+    <header><h1>Avallong</h1></header>
     <nav></nav>
     <aside></aside>
     <section></section>
